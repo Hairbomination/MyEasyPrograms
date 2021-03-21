8ac5b46a74c645f842bb620da1eed47510b1311a
Shannon Griffin <76169649+Hairbomination@users.noreply.github.com>
2021-03-21 15:32:01 +1100

    Update Struct theory test.cpp

--- Struct theory test.cpp
+++ Struct theory test.cpp
@@ -19,6 +19,17 @@ Person getDets()
 	return temp;
 }
 
+void* checkAddresses(void* temp)
+{
+	std::cout << "The integer storing your age is at memory address: " << temp << '\n';
+	temp = static_cast<int*>(temp) + 1;
+	std::cout << "The long value storing your height is at memory address: " << temp << '\n';
+	temp = static_cast<long*>(temp) + 1;
+	std::cout << "The long value storing your weight is at memory address: " << temp << '\n';
+
+	return temp;
+}
+
 int main()
 {
 	std::cout << "Hypothesis: That structs are laid out sequentially in memory. Wrote a program to find "
@@ -27,12 +38,7 @@ int main()
 	std::cout << "Your age is stored in memory address: " << &a.age << '\n' 
 		<< "Your height is stored in memory address: " << &a.height << '\n' 
 		<< "Your weight is stored in memory address: "<< &a.weight << '\n';
-	void* ptr{ &a.age };
-	std::cout << "The integer storing your age is at memory address: " << ptr << '\n';
-	ptr = static_cast<int*>(ptr) + 1;
-	std::cout << "The long value storing your height is at memory address: " << ptr << '\n';
-	ptr = static_cast<long*>(ptr) + 1;
-	std::cout << "The long value storing your weight is at memory address: " << ptr << '\n';
+	void* ptr(checkAddresses(&a.age));
 	if (ptr == &a.weight)
 	{
 		std::cout << "Hypothesis confirmed.";
